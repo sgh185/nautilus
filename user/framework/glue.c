860285c7c5ff4dbb2299912bbc0cb8885d857dc7
Aaron Nelson <arn@arn.xyz>
2020-10-19 17:40:07 -0500

    Update syscall user binary

--- user/framework/glue.c
+++ user/framework/glue.c
@@ -1,14 +1,18 @@
+#include <nautilus/nautilus_exe.h>
+
 void* __gcc_personality_v0;
 
-/// TODO
 void _Unwind_Resume() {
+  nk_vc_printf("Call to _Unwind_Resume\n");
   return;
 }
 
 double __unordtf2() {
+  nk_vc_printf("Call to __unordtf2\n");
   return 0;
 }
 
 void __letf2() {
+  nk_vc_printf("Call to __letf2\n");
   return;
 }
--- user/framework/hello.c
+++ user/framework/hello.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
+#include <unistd.h>
 
-#include <nautilus/nautilus_exe.h>
+// #include <nautilus/nautilus_exe.h>
 
 int main(void *in, void **out)
 {
-    nk_vc_printf("Got here\n");
-    printf("Hello, world, from the user program\n");
-    return 0;
+    // nk_vc_printf("Write using nk_vc_printf\n");
+
+    char write_msg[] = "Write using write\n";
+    write(STDOUT_FILENO, write_msg, sizeof(write_msg));
+
+    /// TODO: figure out why puts and printf cause exceptions
+    puts("Write using putc\n");
+
+    // printf("Write using printf\n");
+    return 42; // To test the return val
 }
