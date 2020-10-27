fd9d42569c59d2ba7c57d5c136acf3ac71555fd3
Brian Suchy (B$) <briansuchy2022@u.northwestern.edu>
2020-10-27 16:35:38 -0500

    Have running (seemingly floating point correct) NAS benchmarks

--- src/nautilus/libccompat.c
+++ src/nautilus/libccompat.c
@@ -730,15 +730,8 @@ if (x<0)
 else
 	return x;
 }
-double sin(double x)
-{
-return x;
-}
+
 double sinh(double x)
-{
-return x;
-}
-double cos(double x)
 {
 return x;
 }
--- src/test/openmp/NPB-NAS/common/wtime.h
+++ src/test/openmp/NPB-NAS/common/wtime.h
@@ -2,6 +2,7 @@
  * You may need to tweak this.
  */
 
+
 #ifndef _WTIME_H_
 #define _WTIME_H_
 
@@ -14,7 +15,24 @@
 #endif
 
 
-#include<nautilus/libccompat.h>
+
+#define time_t      unsigned
+#define suseconds_t unsigned
+struct timeval {
+    time_t      tv_sec;     /* seconds */
+    suseconds_t tv_usec;    /* microseconds */
+};
+
+struct timezone;
+
+static inline int gettimeofdayEDIT(struct timeval *tv, struct timezone *tz_ignored){
+  unsigned long nk_sched_get_realtime();
+  unsigned long ns = nk_sched_get_realtime();
+
+  tv->tv_sec = (time_t) (ns / 1000000000UL);
+  tv->tv_usec = (suseconds_t) ((ns % 100000000UL) / 1000UL);
+  return 0;
+}
 
 
 
