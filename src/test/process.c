3d37eac201161f9c30138b09dd0d2cd3f3092bac
Michael Cuevas <michaelcuevas2021@u.northwestern.edu>
2020-11-17 13:45:07 -0600

    add process test file

---
+++ src/test/process.c
@@ -0,0 +1,36 @@
+
+#include <nautilus/nautilus.h>
+#include <nautilus/process.h>
+#include <nautilus/shell.h>
+#include <nautilus/vc.h>
+
+#define DO_PRINT 1
+
+#if DO_PRINT
+#define PRINT(...) nk_vc_printf(__VA_ARGS__)
+#else
+#define PRINT(...)
+#endif
+
+// create and run a process
+static int
+handle_proctest1 (char * buf, void * priv)
+{
+  nk_process_t *process1;
+  if (nk_process_create("/hello.exe", 0, 0, 0, 0, "paging", &process1)) {
+    nk_vc_printf("handle_proctest1: Failed to create new process\n");
+    return -1;
+  }
+  if (nk_process_run(process1, 0)) {
+    nk_vc_printf("handle_proctest1: Failed to run process\n");
+    return -1;
+  }
+  
+  return 0;
+}
+static struct shell_cmd_impl process_test_create_and_run = {
+  .cmd      = "proctest1",
+  .help_str = "proctest1",
+  .handler  = handle_proctest1,
+};
+nk_register_shell_cmd(process_test_create_and_run);
