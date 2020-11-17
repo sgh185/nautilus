f2a859b7bd653862b8d1fa4c0c0ff35600128bc7
Zhen Huang <zhn1979@peroni.cs.northwestern.edu>
2020-11-16 22:37:56 -0600

    start thread add, bugs exists

--- src/test/openmp/NPB-NAS/paging_benchmark.c
+++ src/test/openmp/NPB-NAS/paging_benchmark.c
@@ -1,5 +1,5 @@
 #include "paging_benchmark.h"
-
+/*
 int paging_wrapper(
     char * _buf, 
     void *_priv,
@@ -76,12 +76,12 @@ int paging_wrapper(
         nk_vc_printf("Survived moving thread into its own address space\n");
     
         nk_vc_printf("Start executing the benchmark\n");
-        
-
+ */     
         /**
          *  What if function don't return at all ??
          * */
-        if((*fptr)(_buf, _priv)){
+/*
+	if((*fptr)(_buf, _priv)){
             nk_vc_printf("Failed running benchmark\n");
             child_ret = 1;
             goto destroy_mas;
@@ -130,3 +130,174 @@ int paging_wrapper(
 
     return 0;
 }
+*/
+struct arg {
+    char * _buf;
+    void *_priv;
+    int (*fptr)(char *, void *);
+    struct nk_virtual_console * vc;
+};
+
+static int thread_func(void *in, void **out){
+
+	struct arg *argument = (struct arg *) in;
+	char * _buf = argument->_buf;
+	void *_priv = argument->_priv;
+	int (*fptr)(char *, void *) = argument->fptr;
+	int ret = 0;
+        get_cur_thread()->vc = argument->vc;
+#ifdef NAUT_CONFIG_ASPACE_PAGING
+	
+	struct nk_thread *t = get_cur_thread();
+	nk_thread_name(t,_buf);
+	nk_aspace_t *old_aspace = t->aspace;
+
+       // nk_vc_printf("The old aspace is %p\n", old_aspace);
+
+        nk_aspace_characteristics_t c;
+       
+        if (nk_aspace_query("paging",&c)) {
+            nk_vc_printf("failed to find paging implementation\n");
+            return 1;
+	}
+
+        // create a new address space for this shell thread
+        nk_aspace_t *mas = nk_aspace_create("paging","paging for NAS benchmark",&c);
+
+        if (!mas) {
+            nk_vc_printf("failed to create new address space\n");
+        }
+        
+        nk_aspace_region_t r;
+        // create a 1-1 region mapping all of physical memory
+        // so that the kernel can work when that thread is active
+        r.va_start = 0;
+        r.pa_start = 0;
+        //r.len_bytes = 0x100000000UL;  // first 4 GB are mapped
+	r.len_bytes = 0x1000000000UL; //first 8 GB are mapped
+        // set protections for kernel
+        // use EAGER to tell paging implementation that it needs to build all these PTs right now
+        r.protect.flags = NK_ASPACE_READ | NK_ASPACE_WRITE | NK_ASPACE_EXEC | NK_ASPACE_PIN | NK_ASPACE_KERN | NK_ASPACE_EAGER;
+
+        // now add the region
+        // this should build the page tables immediately
+        if (nk_aspace_add_region(mas,&r)) {
+            nk_vc_printf("failed to add initial eager region to address space\n");
+            ret = 1;
+            goto destroy_mas;
+        }
+
+        if (nk_aspace_move_thread(mas)) {
+            nk_vc_printf("failed to move shell thread to new address space\n");
+            ret = 1;
+	    goto destroy_mas;
+	}
+    
+        nk_vc_printf("Survived moving thread into its own address space\n");
+    
+        nk_vc_printf("Start executing the benchmark\n");
+	/**
+         *  What if function don't return at all ??
+         * */
+
+
+
+	nk_vc_printf("------------------------------\n");
+	nk_vc_printf("Interface is %p\n",mas->interface);
+	nk_vc_printf("Mov function is %p\n",mas->interface->remove_thread);
+	nk_vc_printf("------------------------------\n");
+
+
+
+
+
+
+
+	if((*fptr)(_buf, _priv)){
+            nk_vc_printf("Failed running benchmark\n");
+            ret = 1;
+            goto destroy_mas;
+        }
+	
+
+
+	nk_vc_printf("------------------------------\n");
+        nk_vc_printf("Interface is %p\n",mas->interface);
+        nk_vc_printf("Mov function is %p\n",mas->interface->remove_thread);
+        nk_vc_printf("------------------------------\n");
+
+
+
+
+        nk_vc_printf("Finish the benchmark\n");
+
+	nk_vc_printf("About to move to the old aspace\n");
+	if(nk_aspace_move_thread(old_aspace) ) {
+            nk_vc_printf("Failed move thread from %p to %p\n", mas, old_aspace);
+            ret = 1;
+            goto destroy_mas;
+        }
+
+        nk_vc_printf("Move thread succeeded\n");
+	
+
+    destroy_mas:
+        if(nk_aspace_destroy(mas)){
+            nk_vc_printf("Something wrong during destorying the new aspace\n");
+            return 1;
+        } else {
+            nk_vc_printf("Destory succeeded\n");
+        }
+        
+    #endif
+   
+    return ret;
+    
+}
+
+int testing(int * in, void ** out){
+	nk_vc_print("The new function is created\n");
+	return 0;
+}
+
+int paging_wrapper(
+    char * _buf, 
+    void *_priv,
+    int (*fptr)(char *, void *)
+){
+	struct arg *args = (struct arg *) malloc(sizeof(struct arg));
+	args->_buf=_buf;
+	args->_priv=_priv;
+	args->fptr=fptr;
+	struct nk_virtual_console * cur_vc = get_cur_thread()->vc;
+	args->vc = cur_vc;
+	if (nk_thread_start(thread_func,
+				args,
+				0,
+				0	,
+				PAGE_SIZE_4KB,
+				NULL,
+				-1)) { 
+		nk_vc_printf("Failed running benchmark\n");
+		nk_join_all_children(0);
+		return 1;
+	    }
+
+
+
+	nk_vc_printf("About to join threads\n");
+	
+        if (nk_join_all_children(0)) {
+                nk_vc_printf("Failed join threads\n");
+                return 1;
+        }
+        
+	nk_vc_printf("Join succeeded\n");
+        nk_sched_reap(1); // clean up unconditionally
+	
+	
+	nk_vc_printf("Succeeded\n");
+	free(args);
+	return 0;    
+}
+    
