3d0c2c187004530960d7d48decd3a058ad579c50
Brian Suchy (B$) <briansuchy2022@u.northwestern.edu>
2020-12-28 11:51:32 -0600

    Added carat stuff for defragmentation

--- src/alloc/dumb/dumb.c
+++ src/alloc/dumb/dumb.c
@@ -29,6 +29,7 @@
 #include <nautilus/shell.h>
 
 #include <nautilus/alloc.h>
+#include <nautilus/aspace.h>
 
 #include <nautilus/mm.h>
 
@@ -48,167 +49,211 @@
 // whenever it allocates a new block to work on
 // it simply leaks the previous one
 struct nk_alloc_dumb {
-    nk_alloc_t *alloc;
+  nk_alloc_t *alloc;
 
-    addr_t block_start;
-    addr_t block_end;
-    addr_t block_cur;
+  addr_t block_start;
+  addr_t block_end;
+  addr_t block_cur;
 
-    uint64_t block_count;
-    uint64_t byte_count;
+  uint64_t block_count;
+  uint64_t byte_count;
+
+  nk_aspace_region_t* region;
 } ;
 
 
 static  int destroy(void *state)
 {
-    struct nk_alloc_dumb *as = (struct nk_alloc_dumb *)state;
-    DEBUG("%s: destroy - note all memory leaked...\n",as->alloc->name);
-    
-    kmem_sys_free(state);  // should call the lowest-level
-    // note that we leak everything else
-    
-    return 0;
+  struct nk_alloc_dumb *as = (struct nk_alloc_dumb *)state;
+  DEBUG("%s: destroy - note all memory leaked...\n",as->alloc->name);
+
+  kmem_sys_free(state);  // should call the lowest-level
+  // note that we leak everything else
+
+  return 0;
 }
-    
-    
+
+
 static int print(void *state, int detailed)
 {
-    struct nk_alloc_dumb *as = (struct nk_alloc_dumb *)state;
-    struct nk_thread *thread = get_cur_thread();
+  struct nk_alloc_dumb *as = (struct nk_alloc_dumb *)state;
+  struct nk_thread *thread = get_cur_thread();
 
-    nk_vc_printf("%s:\n",as->alloc->name);
+  nk_vc_printf("%s:\n",as->alloc->name);
 
-    nk_vc_printf("block_start=%p block_end=%p block_cur=%p block_count=%lu byte_count=%lu\n", as->block_start, as->block_end, as->block_cur, as->block_count,as->byte_count);
+  nk_vc_printf("block_start=%p block_end=%p block_cur=%p block_count=%lu byte_count=%lu\n", as->block_start, as->block_end, as->block_cur, as->block_count,as->byte_count);
 
-    return 0;
-    
+  return 0;
+
 }
 
 
 static inline addr_t round_up_align(addr_t p, size_t a)
 {
-    return p%a ? p+a - p%a : p;
+  return p%a ? p+a - p%a : p;
 }
 
 static inline uint64_t next_pow2(uint64_t n)
 {
-    return 0x1 << (64-(__builtin_clzl(n)+1));
+  return 0x1 << (64-(__builtin_clzl(n)+1));
 }
 
 #define MAX(x,y) ((x)>(y) ? (x) : (y))
 
 static void * impl_alloc(void *state, size_t size, size_t align, int cpu, nk_alloc_flags_t flags)
 {
-    addr_t ret;
-    struct nk_alloc_dumb *as = (struct nk_alloc_dumb *) state;
+  addr_t ret;
+  struct nk_alloc_dumb *as = (struct nk_alloc_dumb *) state;
 
-    DEBUG("%s: alloc size %lu align %lu cpu %d flags=%lx\n",as->alloc->name,size,align,cpu,flags);
-    
-    if ((round_up_align(as->block_cur,align) + size)<as->block_end) {
-	// fits
-	DEBUG("satisfied in current block\n");
-	ret = round_up_align(as->block_cur,align);
-	as->block_cur = ret + size;
-    } else {
-	// no fit
-	uint64_t chunk_size = next_pow2(MAX(size * 2,BASE_BLOCK_SIZE));
+  DEBUG("%s: alloc size %lu align %lu cpu %d flags=%lx\n",as->alloc->name,size,align,cpu,flags);
 
-	DEBUG("allocating new block of size %lu\n",chunk_size);
-	
-	// note simply leaking away previous block...
-	addr_t new = (addr_t) kmem_sys_malloc_specific(chunk_size,cpu,0);
-	if (!new) {
-	    ERROR("Failed to allocate a new block\n");
-	    return 0;
-	}
-	as->block_start= new;
-	as->block_cur = as->block_start;
-	as->block_end = as->block_start + chunk_size;
-	as->block_count++;
-	as->byte_count+=chunk_size;
-
-	return impl_alloc(state,size,align,cpu,flags);
-    }
-
-    if (flags & NK_ALLOC_ZERO) {
-	memset((void*)ret,0,size);
-    }
-
-    DEBUG("%s: returning %p\n",as->alloc->name,(void*)ret);
-
+  if ((round_up_align(as->block_cur,align) + size)<as->block_end) {
+    // fits
+    DEBUG("satisfied in current block\n");
+    ret = round_up_align(as->block_cur,align);
+    as->block_cur = ret + size;
     return (void*)ret;
+  } 
+  //First let us try to defragment the block
+  as->block_cur = defragment_region(as->region);
+  if ((round_up_align(as->block_cur,align) + size)<as->block_end) {
+    // fits
+    DEBUG("satisfied in current block\n");
+    ret = round_up_align(as->block_cur,align);
+    as->block_cur = ret + size;
+    return (void*)ret;
+  }
+  
+  //Defragment failed, time to make a new region
+  
+  uint64_t chunk_size = next_pow2(MAX(size * 2,BASE_BLOCK_SIZE));
+
+  DEBUG("allocating new block of size %lu\n",chunk_size);
+
+  // Create a new arena/region...
+  addr_t new = (addr_t) kmem_sys_malloc_specific(chunk_size,cpu,0);
+
+  if (!new) {
+    ERROR("Failed to allocate a new block\n");
+    return 0;
+  }
+
+  //This is why we should probably merge arenas and regions
+  as->region->va_start = new;
+  as->region->pa_start = new;
+  as->region->len_bytes = chunk_size;
+  as->region->requested_permissions = 0;
+  
+  get_cur_thread()->aspace->interface->add_region(get_cur_thread()->aspace->state, as->region); 
+
+  as->block_start= new;
+  as->block_cur = as->block_start;
+  as->block_end = as->block_start + chunk_size;
+  as->block_count++;
+  as->byte_count+=chunk_size;
+
+  return impl_alloc(state,size,align,cpu,flags);
+
+
+  if (flags & NK_ALLOC_ZERO) {
+    memset((void*)ret,0,size);
+  }
+
+  DEBUG("%s: returning %p\n",as->alloc->name,(void*)ret);
+
+  return (void*)ret;
 }
 
 static void * impl_realloc(void *state, void *ptr, size_t size, size_t align, int cpu, nk_alloc_flags_t flags)
 {
-    struct nk_alloc_dumb *as = (struct nk_alloc_dumb *) state;
+  struct nk_alloc_dumb *as = (struct nk_alloc_dumb *) state;
 
-    DEBUG("%s: realloc %p size %lu align %lu cpu %d flags=%lx\n",as->alloc->name,ptr,size,align,cpu,flags);
+  DEBUG("%s: realloc %p size %lu align %lu cpu %d flags=%lx\n",as->alloc->name,ptr,size,align,cpu,flags);
 
-    void *ret = impl_alloc(state,size,align,cpu,flags);
+  void *ret = impl_alloc(state,size,align,cpu,flags);
 
-    if (!ret) {
-	ERROR("failed to allocate\n");
-	return 0;
-    }
+  if (!ret) {
+    ERROR("failed to allocate\n");
+    return 0;
+  }
 
-    DEBUG("attempting copy.... this is likely to be bogus\n");
-    // we do not really know the size of the old block, so this will end badly...
-    memcpy(ret,ptr,size);
+  DEBUG("attempting copy.... this is likely to be bogus\n");
+  // we do not really know the size of the old block, so this will end badly...
+  memcpy(ret,ptr,size);
 
-    DEBUG("%s: returning %p\n",as->alloc->name,ret);
+  DEBUG("%s: returning %p\n",as->alloc->name,ret);
 
-    return ret;
+  return ret;
 }
 
 static void impl_free(void *state, void *ptr)
 {
-    struct nk_alloc_dumb *as = (struct nk_alloc_dumb *) state;
+  struct nk_alloc_dumb *as = (struct nk_alloc_dumb *) state;
 
-    DEBUG("%s: free %p ignored\n",as->alloc->name,ptr);
+  DEBUG("%s: free %p ignored\n",as->alloc->name,ptr);
 
 }
-    
+
 
 static nk_alloc_interface_t dumb_interface = {
-    .destroy = destroy,
-    .allocp = impl_alloc,
-    .reallocp = impl_realloc,
-    .freep = impl_free,
-    .print = print
+  .destroy = destroy,
+  .allocp = impl_alloc,
+  .reallocp = impl_realloc,
+  .freep = impl_free,
+  .print = print
 };
 
 static struct nk_alloc * create(char *name)
 {
-    DEBUG("create allocator %s\n",name);
-    
-    struct nk_alloc_dumb *as = kmem_sys_malloc_specific(sizeof(*as),my_cpu_id(),1);
-    
-    if (!as) {
-	ERROR("unable to allocate allocator state for %s\n",name);
-	return 0;
-    }
+  DEBUG("create allocator %s\n",name);
 
-    // note that block_start=block_end=block_cur, so first allocation
-    // will trigger a system allocation
-    
-    as->alloc = nk_alloc_register(name,0,&dumb_interface, as);
+  struct nk_alloc_dumb *as = kmem_sys_malloc_specific(sizeof(*as),my_cpu_id(),1);
 
-    if (!as->alloc) {
-	ERROR("Unable to register allocator %s\n",name);
-	kmem_sys_free(as);
-	return 0;
-    }
+  if (!as) {
+    ERROR("unable to allocate allocator state for %s\n",name);
+    return 0;
+  }
 
-    DEBUG("allocator %s configured and initialized\n", as->alloc->name);
+  // note that block_start=block_end=block_cur, so first allocation
+  // will trigger a system allocation
 
-    return as->alloc;
+  as->alloc = nk_alloc_register(name,0,&dumb_interface, as);
+
+  if (!as->alloc) {
+    ERROR("Unable to register allocator %s\n",name);
+    kmem_sys_free(as);
+    return 0;
+  }
+
+  DEBUG("allocator %s configured and initialized\n", as->alloc->name);
+
+ 
+  uint64_t chunk_size = next_pow2(BASE_BLOCK_SIZE);
+  addr_t new = (addr_t) kmem_sys_malloc_specific(chunk_size,cpu,0);
+ 
+  as->region = kmem_sys_malloc_specific(sizeof(nk_aspace_region_t), my_cpu_id(), 1);
+  as->region->protect = 3; 
+  as->region->va_start = new;
+  as->region->pa_start = new;
+  as->region->len_bytes = chunk_size;
+  as->region->requested_permissions = 0;
+  
+  get_cur_thread()->aspace->interface->add_region(get_cur_thread()->aspace->state, as->region); 
+
+  as->block_start= new;
+  as->block_cur = as->block_start;
+  as->block_end = as->block_start + chunk_size;
+  as->block_count++;
+  as->byte_count+=chunk_size;
+
+
+  return as->alloc;
 }
 
 
 static nk_alloc_impl_t dumb = {
-				.impl_name = "dumb",
-				.create = create,
+  .impl_name = "dumb",
+  .create = create,
 };
 
 
