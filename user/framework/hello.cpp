d7ba5bc0ea55759e1d71fabc50d128b2f936df79
Aaron Nelson <arn@arn.xyz>
2021-02-21 17:31:50 -0600

    Add early support for C++ separate compilation

---
+++ user/framework/hello.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include <cstdio>
+
+class HelloFriend {
+public:
+  void hello() {
+    printf("Hello, world!\n");
+  }
+}
+
+int main() {
+  HelloFriend fnd;
+  fnd.hello();
+  // std::cout << "Hello, world!" << std::endl;
+  return 0;
+}
