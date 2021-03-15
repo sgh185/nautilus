2e565ec38518b39acfc4f0857252e591abba619e
Zhen Huang <zhn1979@peroni.cs.northwestern.edu>
2021-03-14 20:41:53 -0500

    updates for PARSEC

--- user/framework/glue.c
+++ user/framework/glue.c
@@ -1,20 +1,5 @@
 #include <stdio.h>
 
-void* __gcc_personality_v0;
-
-void _Unwind_Resume() {
-  printf("Call to fake _Unwind_Resume\n");
-  return;
-}
-
-void _Unwind_GetCFA() {
-  return;
-}
-
-void _Unwind_ForcedUnwind() {
-  
-}
-
 double __unordtf2() {
   printf("Call to fake __unordtf2\n");
   return 0;
