bbd7db2544c8bada13a1dc0f21f771c5b665f3bb
Souradip Ghosh <sgh@u.northwestern.edu>
2021-05-04 20:04:50 +0000

    user framework: fix nld compilation of c++ programs, add -lgcc_eh, remove glue code

--- user/framework/glue.c
+++ user/framework/glue.c
@@ -2,6 +2,7 @@
 
 void* __gcc_personality_v0;
 
+#if 0
 __attribute__((used))
 void _Unwind_Resume() {
   printf("Call to fake _Unwind_Resume\n");
@@ -23,3 +24,4 @@ double __unordtf2() {
   printf("Call to fake __unordtf2\n");
   return 0;
 }
+#endif
